4fb7df30d6b07a13fcf0577a6bc0a671d52e5dc2
abdraheem98 <abdulraheem.akv@gmail.com>
2026-05-15 08:07:03 +0530

    file for master

---
+++ mergeconflict/app.js
@@ -0,0 +1,9 @@
+const greet = (name) => `Hello, ${name}!`;
+const farewell = (name) => `Bye, ${name}!`;
+console.log(greet("World"));
+console.log(farewell("World"));
+
+if (require.main === module) {
+    console.log(greet("World"));
+    console.log(farewell("World"));
+}
